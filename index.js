73ca5e867e98d4061d54f725fbfe40f99d09c5e2
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-06-15 02:11:12 -0700

    added more functions for adding and viewing the database.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const initQs = require('./lib/questions');
 const q = require('./lib/query');
 
 
-
+//this allows me to connect to the sql database
 const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
     password: 'rootroot',
     database: 'employees_db'
 });
-
+// this makes the connection in the terminal to the db
 db.connect((err) => {
     if (err) {
         console.error('Error message:', err);
@@ -21,6 +21,7 @@ db.connect((err) => {
     console.log(`connected to database`)
 })
 
+//write code to convert role or department name into role_id or department_id
 const viewEmployees = () => {
     //id, first, last, title(role), department, salary, manager
     db.query(q.viewEmployees, (err, result) => {
@@ -45,6 +46,82 @@ const viewDeparts = () => {
         if (err) console.error(err);
         console.table(result);
         init();
+    })
+}
+const addRole = async () => {
+    let departList = [];
+    db.query(q.departList, (err, result) => {
+        if (err) console.log(err);
+        for (let choice of result) departList.push(choice);
+    })
+    let newRole = await inquirer.prompt([{
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of this role?'
+        },
+        {
+            type: 'list',
+            name: 'depart',
+            message: 'What department does it belong to?',
+            choices: departList
+        }
+    ]);
+    db.query(q.addRole, [newRole.roleName, newRole.salary, 3], (err, result) => {
+        if (err) console.log(err);
+        console.log(`added${newRole.roleName} to roles`);
+        init();
+    })
+}
+
+const addEmployee = () => {
+    let roleList = [];
+    db.query(q.rolesList, (err, result) => {
+        if (err) console.log(err);
+        for (let choice of result) {
+            roleList.push(choice.title);
+        }
+    });
+    inquirer.prompt([{
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the employees first name?'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the employees last name?'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is their position?',
+            choices: roleList
+        }
+    ]).then((data) => {
+        db.query(q.addEmployee, [data.first_name, data.last_name, data.role], (err, result) => {
+            if (err) console.error(err);
+            console.log(`added ${data.first_name} ${data.last_name} to employees`);
+            init();
+        })
+    })
+}
+
+const addDepart = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'department',
+        message: 'What is the name of the department?'
+    }]).then((data) => {
+        db.query(q.addDepart, data.department, (err, result) => {
+            if (err) console.error(err);
+            console.log(`added ${data.department} to departments`);
+            init();
+        })
     })
 }
 
@@ -77,7 +154,7 @@ const init = async () => {
             addDepart();
             break;
         case 'quit':
-            break;
+            process.exit();
     }
 };
 
